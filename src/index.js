c652ff3356d446d63527098453e052d793051fa4
SvetlanaDrozdova <sdrozdova032@gmail.com>
2021-08-25 13:42:18 +0400

    correct the function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 
 // You should implement your task here.
-let array = [];
+
 module.exports = function towelSort (matrix) {
+  /*for (let j = 0; j < matrix.length; j++){
+    if (j % 2 !== 0){
+      matrix[j].reverse();
+    }
+  }*/
+  let array = [];
   for (let j = 0; j < matrix.length; j++){
     if (j % 2 !== 0){
       matrix[j].reverse();
     }
-  }
-  for (let j = 0; j < matrix.length; j++){
     for (let i = 0; i < matrix[j].length; i++){
-      array.push(matrix[j][i]);
+     array.push(matrix[j][i]);
     }
   }
- return array;
+  return array;
 }
